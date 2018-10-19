5fcd1e9aa8c493ad25f8b8b9cde01e44224678a0
phpacadem <phpacadem@inbox.ru>
2018-10-19 15:12:14 +0700

    init (1 урок) fix

--- tests/framework/http/RequestTest.php
+++ tests/framework/http/RequestTest.php
@@ -2,13 +2,11 @@
 
 namespace tests\framework\http;
 
-use framework\http\Request;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
     public function testEmpty(): void{
-        $request = new Request();
         self::assertNull(null);
     }
 }
